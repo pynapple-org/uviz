cc61cbac9451c56a29b8fb051b1de3cdc592e50f
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-05-15 18:07:24 -0400

    update only when needed

--- src/pynaviz/controller.py
+++ src/pynaviz/controller.py
@@ -7,7 +7,7 @@ from pygfx.cameras._perspective import fov_distance_factor
 import pylinalg as la
 
 
-class UpdateEvent(Event):
+class SyncEvent(Event):
 	"""Keyboard button press.
 
 	Parameters
@@ -28,6 +28,28 @@ class UpdateEvent(Event):
 		super().__init__(*args, **kwargs)
 		self.controller_id = controller_id
 		self.data = data
+
+class SetStateAndDrawEvent(Event):
+	"""Keyboard button press.
+
+	Parameters
+	----------
+	args :  Any
+		Positional arguments are forwarded to the :class:`base class
+		<pygfx.objects.Event>`.
+	key : str
+		The key that was pressed.
+	modifiers : list
+		The modifiers that were pressed while the key was pressed.
+	kwargs : Any
+		Additional keyword arguments are forward to the :class:`base class
+		<pygfx.objects.Event>`.
+
+	"""
+	def __init__(self, *args, controller_id=None, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.controller_id = controller_id
+
 
 
 class ControllerGroup:
@@ -52,6 +74,8 @@ class ControllerGroup:
 	def _add_update_handler(self, viewport_or_renderer: Union[Viewport, Renderer]):
 		viewport = Viewport.from_viewport_or_renderer(viewport_or_renderer)
 		viewport.renderer.add_event_handler(self.update, "update")
+		viewport.renderer.add_event_handler(self.update, "set_and_draw")
+
 
 	def add(self, controller):
 		pass
@@ -72,6 +96,11 @@ class ControllerGroup:
 			elif update_type == "zoom_to_point":
 				ctrl.compensate_zoom_to_point(*event.data["args"], **event.data["kwargs"])
 
+	def set_camera_state_and_draw(self, event):
+		for id_other, ctrl in self._controller_group.items():
+			if event.controller_id == id_other:
+				continue
+			ctrl.set_camera_state_and_draw()
 
 class PynaVizController(PanZoomController):
 	def __init__(
@@ -116,7 +145,7 @@ class PynaVizController(PanZoomController):
 			"kwargs": kwargs
 		}
 		if self.renderer_handle_event:
-			self.renderer_handle_event(UpdateEvent("update", controller_id=self._controller_id, data=ev))
+			self.renderer_handle_event(SyncEvent("update", controller_id=self._controller_id, data=ev))
 
 	def _update_pan(self, delta, *, vecx, vecy):
 		super()._update_pan(delta, vecx=vecx, vecy=vecy)
@@ -170,5 +199,55 @@ class PynaVizController(PanZoomController):
 
 		# Update camera
 		self._set_camera_state({"position": new_position, "width": cam_state["width"]})
+
+
+	def handle_event(self, event, viewport):
+		super().handle_event(event, viewport)
+		need_update = False
+		type = event.type
+		if type.startswith(("pointer_", "key_", "wheel")):
+			modifiers = {m.lower() for m in event.modifiers}
+			if type.startswith("key_"):
+				modifiers.discard(event.key.lower())
+			modifiers_prefix = "+".join(sorted(modifiers) + [""])
+
+		if type == "before_render":
+			if self._auto_update and self._actions:
+				need_update = True
+		elif type == "pointer_down" and viewport.is_inside(event.x, event.y):
+			# Start a drag, or an action with mode push/peek/repeat
+			key = modifiers_prefix + f"mouse{event.button}"
+			action_tuple = self._controls.get(key)
+			if action_tuple:
+				need_update = True
+			# Update all drag actions
+			for action in self._actions.values():
+				if action.mode == "drag" and not action.done:
+					need_update = True
+		elif type == "pointer_up":
+			need_update = self._handle_button_up(f"mouse{event.button}")
+		elif type == "wheel" and viewport.is_inside(event.x, event.y):
+			# Wheel events. Technically there is horizontal and vertical scroll,
+			# but this does not work well cross-platform, so we consider it 1D.
+			key = modifiers_prefix + "wheel"
+			action_tuple = self._controls.get(key)
+			if action_tuple:
+				need_update = True
+
+		elif type == "key_down":
+			# Start an action with mode push/peek/repeat
+			key = modifiers_prefix + f"{event.key.lower()}"
+			action_tuple = self._controls.get(key)
+			if action_tuple:
+				need_update = True
+
+		elif type == "key_up":
+			# End key presses, regardless of modifier state
+			need_update = self._handle_button_up(f"{event.key.lower()}")
+
+		if need_update:
+			self.renderer_handle_event(SetStateAndDrawEvent("set_and_draw", controller_id=self._controller_id))
+
+	def set_camera_state_and_draw(self):
 		self._update_cameras()
 		self._draw()
