f43796397d288ba1a92b5ea2af4d3bb1cc58d8c3
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-05-16 12:46:34 -0400

    added test for event init

---
+++ tests/test_events.py
@@ -0,0 +1,12 @@
+import pytest
+import pynaviz.events as events
+
+
+def test_sync_event_initialization():
+    ev = events.SyncEvent("mytype", update_type="up", controller_id=1, data={"args": (1, 2, 3), "kwargs": dict(one=1, two=2)})
+    assert ev.type == "mytype"
+    assert ev.update_type == "up"
+    assert ev.controller_id == 1
+    assert ev.args == (1, 2, 3)
+    assert tuple(ev.kwargs.values()) == (1, 2)
+    assert tuple(ev.kwargs.keys()) == ("one", "two")
