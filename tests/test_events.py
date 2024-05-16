3eb809cd1d2f73f8b736177a52a9773e0b86cd66
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-05-16 13:57:15 -0400

    fixed tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -3,7 +3,7 @@ import pynaviz.events as events
 
 
 def test_sync_event_initialization():
-    ev = events.SyncEvent("mytype", update_type="up", controller_id=1, data={"args": (1, 2, 3), "kwargs": dict(one=1, two=2)})
+    ev = events.SyncEvent("mytype", update_type="up", controller_id=1, sync_extra_args={"args": (1, 2, 3), "kwargs": dict(one=1, two=2)})
     assert ev.type == "mytype"
     assert ev.update_type == "up"
     assert ev.controller_id == 1
