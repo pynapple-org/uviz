5b0ee11a565155c60f226a3346750f825d4eb1a6
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-05-16 10:09:17 -0400

    use tabs

--- src/pynaviz/controller.py
+++ src/pynaviz/controller.py
@@ -7,174 +7,173 @@ from typing import Callable, Optional, Union
 from pygfx import Camera, PanZoomController, Renderer, Viewport
 
 from .events import SyncEvent
-from .sync_controller_callbacks import _match_pan_on_x_axis, _match_zoom_on_x_axis
 
 
 class ControllerGroup:
 
-    def __init__(self, *controllers_and_renderers):
-        self._controller_group = dict()
-        ids = [
-            ctrl.controller_id
-            for ctrl, _ in controllers_and_renderers
-            if ctrl.controller_id is not None
-        ]
-        if len(set(ids)) != len(ids):
-            raise ValueError("Controller ids must be all different!")
-        if ids:
-            id0 = max(ids) + 1
-        else:
-            id0 = 0
+	def __init__(self, *controllers_and_renderers):
+		self._controller_group = dict()
+		ids = [
+			ctrl.controller_id
+			for ctrl, _ in controllers_and_renderers
+			if ctrl.controller_id is not None
+		]
+		if len(set(ids)) != len(ids):
+			raise ValueError("Controller ids must be all different!")
+		if ids:
+			id0 = max(ids) + 1
+		else:
+			id0 = 0
 
-        for i, cntrl_and_rend in enumerate(controllers_and_renderers):
-            ctrl, rend = cntrl_and_rend
-            if ctrl.controller_id is None:
-                ctrl.controller_id = i + id0
-            self._controller_group[ctrl.controller_id] = ctrl
-            self._add_update_handler(rend)
+		for i, cntrl_and_rend in enumerate(controllers_and_renderers):
+			ctrl, rend = cntrl_and_rend
+			if ctrl.controller_id is None:
+				ctrl.controller_id = i + id0
+			self._controller_group[ctrl.controller_id] = ctrl
+			self._add_update_handler(rend)
 
-    def _add_update_handler(self, viewport_or_renderer: Union[Viewport, Renderer]):
-        viewport = Viewport.from_viewport_or_renderer(viewport_or_renderer)
-        viewport.renderer.add_event_handler(self.update, "update")
+	def _add_update_handler(self, viewport_or_renderer: Union[Viewport, Renderer]):
+		viewport = Viewport.from_viewport_or_renderer(viewport_or_renderer)
+		viewport.renderer.add_event_handler(self.update, "update")
 
-    def add(self, controller):
-        pass
+	def add(self, controller):
+		pass
 
-    def remove(self, controller_id):
-        pass
+	def remove(self, controller_id):
+		pass
 
-    def update(self, event):
-        # print(f"update controller {event.controller_id}")
-        for id_other, ctrl in self._controller_group.items():
-            if event.controller_id == id_other:
-                continue
-            ctrl.sync(event)
+	def update(self, event):
+		# print(f"update controller {event.controller_id}")
+		for id_other, ctrl in self._controller_group.items():
+			if event.controller_id == id_other:
+				continue
+			ctrl.sync(event)
 
 
 class PynaVizController(PanZoomController):
-    def __init__(
-        self,
-        camera: Optional[Camera] = None,
-        *,
-        enabled=True,
-        damping: int = 4,
-        auto_update: bool = True,
-        register_events: Optional[Union[Viewport, Renderer]] = None,
-        controller_id: Optional[int] = None,
-        dict_sync_funcs: Optional[dict[Callable]] = None,
-    ):
+	def __init__(
+		self,
+		camera: Optional[Camera] = None,
+		*,
+		enabled=True,
+		damping: int = 4,
+		auto_update: bool = True,
+		register_events: Optional[Union[Viewport, Renderer]] = None,
+		controller_id: Optional[int] = None,
+		dict_sync_funcs: Optional[dict[Callable]] = None,
+	):
 
-        self._controller_id = controller_id
+		self._controller_id = controller_id
 
-        super().__init__(
-            camera=camera,
-            enabled=enabled,
-            damping=damping,
-            auto_update=auto_update,
-            register_events=register_events,
-        )
-        self.renderer_handle_event = None
-        self._draw = lambda: True
+		super().__init__(
+			camera=camera,
+			enabled=enabled,
+			damping=damping,
+			auto_update=auto_update,
+			register_events=register_events,
+		)
+		self.renderer_handle_event = None
+		self._draw = lambda: True
 
-        if register_events:
-            self.renderer_handle_event = self._get_event_handle(register_events)
-            self._draw = lambda: self._request_draw(register_events)
+		if register_events:
+			self.renderer_handle_event = self._get_event_handle(register_events)
+			self._draw = lambda: self._request_draw(register_events)
 
-        if dict_sync_funcs is None:
-            self._dict_sync_funcs = dict()
-        else:
-            self._dict_sync_funcs = dict_sync_funcs
+		if dict_sync_funcs is None:
+			self._dict_sync_funcs = dict()
+		else:
+			self._dict_sync_funcs = dict_sync_funcs
 
-    @property
-    def controller_id(self):
-        return self._controller_id
+	@property
+	def controller_id(self):
+		return self._controller_id
 
-    @controller_id.setter
-    def controller_id(self, value):
-        if self._controller_id is not None:
-            raise ValueError("Controller id can be set only once!")
-        self._controller_id = value
+	@controller_id.setter
+	def controller_id(self, value):
+		if self._controller_id is not None:
+			raise ValueError("Controller id can be set only once!")
+		self._controller_id = value
 
-    @staticmethod
-    def _get_event_handle(register_events: Union[Viewport, Renderer]) -> Callable:
-        """
-        Set up the callback to update.
+	@staticmethod
+	def _get_event_handle(register_events: Union[Viewport, Renderer]) -> Callable:
+		"""
+		Set up the callback to update.
 
-        :return:
-        """
-        # grab the viewport
-        viewport = Viewport.from_viewport_or_renderer(register_events)
-        return viewport.renderer.handle_event
+		:return:
+		"""
+		# grab the viewport
+		viewport = Viewport.from_viewport_or_renderer(register_events)
+		return viewport.renderer.handle_event
 
-    def _request_draw(self, viewport):
-        if self.auto_update:
-            viewport = Viewport.from_viewport_or_renderer(viewport)
-            viewport.renderer.request_draw()
+	def _request_draw(self, viewport):
+		if self.auto_update:
+			viewport = Viewport.from_viewport_or_renderer(viewport)
+			viewport.renderer.request_draw()
 
-    def _update_event(self, update_type: str, *args, **kwargs):
-        if self.renderer_handle_event:
-            data = dict(args=args, kwargs=kwargs)
-            self.renderer_handle_event(
-                SyncEvent(
-                    "update",
-                    controller_id=self._controller_id,
-                    update_type=update_type,
-                    data=data,
-                )
-            )
+	def _update_event(self, update_type: str, *args, **kwargs):
+		if self.renderer_handle_event:
+			data = dict(args=args, kwargs=kwargs)
+			self.renderer_handle_event(
+				SyncEvent(
+					"update",
+					controller_id=self._controller_id,
+					update_type=update_type,
+					data=data,
+				)
+			)
 
-    def _update_pan(self, delta, *, vecx, vecy):
-        super()._update_pan(delta, vecx=vecx, vecy=vecy)
-        self._update_event(
-            update_type="pan",
-            cam_state=self._get_camera_state(),
-            delta=delta,
-            vecx=vecx,
-            vecy=vecy,
-        )
+	def _update_pan(self, delta, *, vecx, vecy):
+		super()._update_pan(delta, vecx=vecx, vecy=vecy)
+		self._update_event(
+			update_type="pan",
+			cam_state=self._get_camera_state(),
+			delta=delta,
+			vecx=vecx,
+			vecy=vecy,
+		)
 
-    def _update_zoom(self, delta):
-        super()._update_zoom(delta)
-        self._update_event(
-            update_type="zoom", cam_state=self._get_camera_state(), delta=delta
-        )
+	def _update_zoom(self, delta):
+		super()._update_zoom(delta)
+		self._update_event(
+			update_type="zoom", cam_state=self._get_camera_state(), delta=delta
+		)
 
-    def _update_zoom_to_point(self, delta, *, screen_pos, rect):
-        super()._update_zoom_to_point(delta, screen_pos=screen_pos, rect=rect)
-        self._update_event(
-            update_type="zoom_to_point",
-            cam_state=self._get_camera_state(),
-            delta=delta,
-            screen_pos=screen_pos,
-            rect=rect,
-        )
+	def _update_zoom_to_point(self, delta, *, screen_pos, rect):
+		super()._update_zoom_to_point(delta, screen_pos=screen_pos, rect=rect)
+		self._update_event(
+			update_type="zoom_to_point",
+			cam_state=self._get_camera_state(),
+			delta=delta,
+			screen_pos=screen_pos,
+			rect=rect,
+		)
 
-    def sync_pan(self, *args, **kwargs):
-        cam_state = kwargs["cam_state"]
-        x_pos = cam_state["position"][0]
-        width = cam_state["width"]
+	def sync_pan(self, *args, **kwargs):
+		cam_state = kwargs["cam_state"]
+		x_pos = cam_state["position"][0]
+		width = cam_state["width"]
 
-        self_camera_state = self._get_camera_state()
-        self_x_pos = self_camera_state["position"][0]
-        self_width = self_camera_state["width"]
-        # get the dx
-        dx = (x_pos / width - self_x_pos / self_width) * self_width
+		self_camera_state = self._get_camera_state()
+		self_x_pos = self_camera_state["position"][0]
+		self_width = self_camera_state["width"]
+		# get the dx
+		dx = (x_pos / width - self_x_pos / self_width) * self_width
 
-        new_position = self_camera_state["position"].copy()
-        new_position[0] = new_position[0] + dx
+		new_position = self_camera_state["position"].copy()
+		new_position[0] = new_position[0] + dx
 
-        # Update camera
-        self._set_camera_state({"position": new_position})
-        self._update_cameras()
-        self._draw()
+		# Update camera
+		self._set_camera_state({"position": new_position})
+		self._update_cameras()
+		self._draw()
 
-    def sync(self, event):
-        if event.update_type in self._dict_sync_funcs:
-            func = self._dict_sync_funcs[event.update_type]
-            state_update = func(event, self._get_camera_state())
-        else:
-            raise NotImplemented(f"Update {event.update_type} not implemented!")
-        # Update camera
-        self._set_camera_state(state_update)
-        self._update_cameras()
-        self._draw()
+	def sync(self, event):
+		if event.update_type in self._dict_sync_funcs:
+			func = self._dict_sync_funcs[event.update_type]
+			state_update = func(event, self._get_camera_state())
+		else:
+			raise NotImplemented(f"Update {event.update_type} not implemented!")
+		# Update camera
+		self._set_camera_state(state_update)
+		self._update_cameras()
+		self._draw()
