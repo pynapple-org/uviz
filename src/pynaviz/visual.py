b6c2d6301480e0a51dbff84626f183c714ab9721
Caitlin <caitlin9165@gmail.com>
2024-07-15 15:46:13 -0400

    add setup.py and version

--- src/pynaviz/visual.py
+++ src/pynaviz/visual.py
@@ -1,9 +1,14 @@
 from typing import Tuple
 
+import fastplotlib as fpl
+
 
 class Visual:
     def __init__(
-            self,
-            shape: Tuple[int, int] = (1, 1)
+            self
     ):
-        pass
+        self._figure = fpl.Figure()
+
+    @property
+    def figure(self) -> fpl.Figure:
+        return self._figure
