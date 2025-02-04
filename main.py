be55c3847a67d4a85e88029e88bc60c81e8b3b0f
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-02-04 15:50:37 -0500

    Update

--- main.py
+++ main.py
@@ -11,4 +11,10 @@ tsd1 = nap.Tsd(t=np.arange(1000), d=np.cos(np.arange(1000) * 0.1))
 tsd2 = nap.Tsd(t=np.arange(1000), d=np.cos(np.arange(1000) * 0.1))
 tsd3 = nap.Tsd(t=np.arange(1000), d=np.arange(1000))
 
+tsg = nap.TsGroup({
+    i:nap.Ts(
+        t=np.sort(np.random.uniform(0, 1000, 100*(i+1)))
+    ) for i in range(10)
+})
+
 scope(globals())
--- src/pynaviz/pynaviz.py
+++ src/pynaviz/pynaviz.py
@@ -78,43 +78,144 @@ DOCK_LIST_STYLESHEET = '''
 '''
 
 
-class TsdCanvas(QDockWidget):
-    def __init__(self):
-        super().__init__(None)
-        self.resize(640, 480)
+class TsGroupView(QDockWidget):
 
-        # Creat button and hook it up
-        self._button = QPushButton("Add a line", self)
-        self._button.clicked.connect(self._on_button_click)
+    def __init__(self, tsgroup):
+        super().__init__("TsGroup")
 
-        # Create canvas, renderer and a scene object
+        self.setAllowedAreas(Qt.DockWidgetArea.RightDockWidgetArea)
+
+        tsd = tsgroup.to_tsd()
+
+        positions = np.stack((tsd.t, tsd.d, np.zeros_like(tsd))).T
+        positions = positions.astype('float32')
+
+        # # Create canvas, renderer and a scene object
         self._canvas = WgpuCanvas(parent=self)
         self._renderer = gfx.WgpuRenderer(self._canvas)
         self._scene = gfx.Scene()
-        self._camera = gfx.OrthographicCamera(110, 110)
 
-        # Hook up the animate callback
+        background = gfx.Background.from_color("#ddd")
+
+        self._grid = gfx.Grid(
+            None,
+            gfx.GridMaterial(
+                major_step=10,
+                minor_step=1,
+                thickness_space="screen",
+                major_thickness=2,
+                minor_thickness=0.5,
+                infinite=True,
+            ),
+            orientation="xy",
+        )
+        self._grid.local.z = -1001
+
+        self.rulerx = gfx.Ruler(tick_side="right")
+        self.rulery = gfx.Ruler(tick_side="left", min_tick_distance=40)
+
+        points = gfx.Points(
+            gfx.Geometry(positions=positions),
+            gfx.PointsMaterial(size=5, color="#066", opacity=0.5),
+        )
+
+        self._scene.add(background, self._grid, self.rulerx, self.rulery, points)
+
+        self._camera = gfx.OrthographicCamera(maintain_aspect=False)
+        self._camera.show_rect(-100, 1100, -5, 5)
+
+        self._controller = gfx.PanZoomController(self._camera, register_events=self._renderer)
+
+        self.setWidget(self._canvas)
+
+        # # Hook up the animate callback
         self._canvas.request_draw(self.animate)
 
-        layout = QHBoxLayout()
-        self.setLayout(layout)
-        layout.addWidget(self._button)
-        layout.addWidget(self._canvas)
 
-    def _on_button_click(self):
-        positions = [
-            [random.uniform(-50, 50), random.uniform(-50, 50), 0] for i in range(8)
-        ]
-        line = gfx.Line(
-            gfx.Geometry(positions=positions), gfx.LineMaterial(thickness=3)
-        )
-        self._scene.add(line)
-        self._canvas.update()
+    def map_screen_to_world(self, pos, viewport_size):
+        # first convert position to NDC
+        x = pos[0] / viewport_size[0] * 2 - 1
+        y = -(pos[1] / viewport_size[1] * 2 - 1)
+        pos_ndc = (x, y, 0)
+
+        pos_ndc += vec_transform(self._camera.world.position, self._camera.camera_matrix)
+        # unproject to world space
+        pos_world = vec_unproject(pos_ndc[:2], self._camera.camera_matrix)
+
+        return pos_world
 
     def animate(self):
+        print(self._renderer.logical_size)
+        # get range of screen space
+        xmin, ymin = 0, self._renderer.logical_size[1]
+        xmax, ymax = self._renderer.logical_size[0], 0
+
+        world_xmin, world_ymin, _ = self.map_screen_to_world((xmin, ymin), self._renderer.logical_size)
+        world_xmax, world_ymax, _ = self.map_screen_to_world((xmax, ymax), self._renderer.logical_size)
+
+        # set start and end positions of rulers
+        self.rulerx.start_pos = world_xmin, 0, -1000
+        self.rulerx.end_pos = world_xmax, 0, -1000
+
+        self.rulerx.start_value = self.rulerx.start_pos[0]
+
+        statsx = self.rulerx.update(self._camera, self._canvas.get_logical_size())
+
+        self.rulery.start_pos = 0, world_ymin, -1000
+        self.rulery.end_pos = 0, world_ymax, -1000
+
+        self.rulery.start_value = self.rulery.start_pos[1]
+        statsy = self.rulery.update(self._camera, self._canvas.get_logical_size())
+
+        major_step_x, major_step_y = statsx["tick_step"], statsy["tick_step"]
+        self._grid.material.major_step = major_step_x, major_step_y
+        self._grid.material.minor_step = 0.2 * major_step_x, 0.2 * major_step_y
+
+        # print(statsx)
         self._renderer.render(self._scene, self._camera)
 
+    def _create_title_bar(self):
+        """Create the title bar."""
+        self._title_bar = QWidget(self)
 
+        self._layout = QHBoxLayout(self._title_bar)
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self._layout.setSpacing(0)
+
+        self._title_bar.setStyleSheet(DOCK_TITLE_STYLESHEET)
+
+        # Left part of the bar.
+        # ---------------------
+
+        # Widget name.
+        label = QLabel(self.windowTitle())
+        self._layout.addWidget(label)
+
+        # Space.
+        # ------
+        self._layout.addStretch(1)
+
+        # Layout margin.
+        self._title_bar.setLayout(self._layout)
+        self.setTitleBarWidget(self._title_bar)
+
+    def _create_status_bar(self):
+        # Dock has requested widget and status bar.
+        widget_container = QWidget(self)
+        widget_layout = QVBoxLayout(widget_container)
+        widget_layout.setContentsMargins(0, 0, 0, 0)
+        widget_layout.setSpacing(0)
+
+        widget_layout.addWidget(self.listWidget, 100)
+
+        # Widget status text.
+        self._status = QLabel('')
+        self._status.setMaximumHeight(30)
+        self._status.setStyleSheet(DOCK_STATUS_STYLESHEET)
+        widget_layout.addWidget(self._status, 1)
+
+        widget_container.setLayout(widget_layout)
+        self.setWidget(widget_container)
 
 
 class TsdView(QDockWidget):
@@ -132,7 +233,7 @@ class TsdView(QDockWidget):
         self._renderer = gfx.WgpuRenderer(self._canvas)
         self._scene = gfx.Scene()
 
-        background = gfx.Background.from_color("#000")
+        background = gfx.Background.from_color("#ddd")
 
         self._grid = gfx.Grid(
             None,
@@ -153,7 +254,7 @@ class TsdView(QDockWidget):
 
         line = gfx.Line(
             gfx.Geometry(positions=positions),
-            # gfx.LineMaterial(thickness=4.0, color="#aaf"),
+            gfx.LineMaterial(thickness=4.0, color="#aaf"),
         )
 
         self._scene.add(background, self._grid, self.rulerx, self.rulery, line)
@@ -169,8 +270,6 @@ class TsdView(QDockWidget):
         self._canvas.request_draw(self.animate)
 
 
-
-
     def map_screen_to_world(self, pos, viewport_size):
         # first convert position to NDC
         x = pos[0] / viewport_size[0] * 2 - 1
@@ -184,6 +283,7 @@ class TsdView(QDockWidget):
         return pos_world
 
     def animate(self):
+        print(self._renderer.logical_size)
         # get range of screen space
         xmin, ymin = 0, self._renderer.logical_size[1]
         xmax, ymax = self._renderer.logical_size[0], 0
@@ -288,7 +388,7 @@ class LeftDock(QDockWidget):
         var = self.pynavar[item.text()]
 
         if isinstance(var, nap.TsGroup):
-            self.add_raster_view(var, item.text())
+            self.add_tsgroup_view(var, item.text())
         elif isinstance(var, nap.Tsd):
             self.add_tsd_view(var, item.text())
         elif isinstance(var, nap.TsdFrame):
@@ -296,23 +396,17 @@ class LeftDock(QDockWidget):
 
         return
 
-    # def add_raster_view(self, tsgroup, name):
-    #     group_times = np.hstack([tsgroup[k].index.values for k in tsgroup.keys()])
-    #     group_clusters = np.hstack([
-    #         np.ones(len(tsgroup[k]), dtype='int') * k for k in tsgroup.keys()
-    #     ])
-    #     cluster_ids = np.unique(group_clusters)
-    #
-    #     view = TsGroupView(group_times, group_clusters, cluster_ids=cluster_ids)
-    #     view.plot()
-    #     view.attach(self.gui)
-    #     self.views[name] = view
-    #     return
+    def add_tsgroup_view(self, tsgroup, name):
+        print("Adding tsgroup")
+        view = TsGroupView(tsgroup)
+        self.gui.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, view)
+
+        self.views[name] = view
+        return
 
     def add_tsd_view(self, tsd, name):
         print("Adding tsd")
         view = TsdView(tsd)
-        # view = TsdCanvas()
         self.gui.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, view)
         # view.show()
         # view.plot()
@@ -366,6 +460,9 @@ class LeftDock(QDockWidget):
 
         widget_container.setLayout(widget_layout)
         self.setWidget(widget_container)
+
+
+
 
 class GUI(QMainWindow):
     def __init__(self):
