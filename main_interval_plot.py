867cf755ed4b2889950f47e9b4ba52b939184810
sjvenditto <sjvenditto@gmail.com>
2025-06-09 12:10:49 -0400

    Update main_interval_plot.py

--- main_interval_plot.py
+++ main_interval_plot.py
@@ -7,18 +7,30 @@ from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QPushButton
 
 
 tsd1 = nap.Tsd(t=np.arange(1000), d=np.cos(np.arange(1000) * 0.1))
-tsg = nap.TsGroup({
-    i: nap.Ts(
-        t=np.linspace(0, 100, 100 * (10 - i))
-    ) for i in range(10)},
+tsg = nap.TsGroup(
+    {i: nap.Ts(t=np.linspace(0, 100, 100 * (10 - i))) for i in range(10)},
     metadata={
         "label": np.arange(10),
-        "colors": ["hotpink", "lightpink", "cyan", "orange", "lightcoral", "lightsteelblue", "lime", "lightgreen",
-                   "magenta", "pink"],
+        "colors": [
+            "hotpink",
+            "lightpink",
+            "cyan",
+            "orange",
+            "lightcoral",
+            "lightsteelblue",
+            "lime",
+            "lightgreen",
+            "magenta",
+            "pink",
+        ],
         "test": ["hello", "word"] * 5,
-    }
+    },
 )
-tsdframe = nap.TsdFrame(t=np.arange(1000), d=np.random.randn(1000, 10), metadata={"label": np.random.randn(10)})
+tsdframe = nap.TsdFrame(
+    t=np.arange(1000),
+    d=np.random.randn(1000, 10),
+    metadata={"label": np.random.randn(10)},
+)
 tsdtensor = nap.TsdTensor(t=np.arange(1000), d=np.random.randn(1000, 10, 10))
 
 app = QApplication([])
@@ -31,11 +43,13 @@ v.plot.controller.show_interval(0, 20)
 ep = [nap.IntervalSet(35, 50), nap.IntervalSet(30, 40)]
 v.plot.add_interval_sets(ep, colors=["cyan", "blue"])
 
-v.plot.add_interval_sets(nap.IntervalSet([10, 80], [20,100]), colors="purple", alpha=0.3)
+v.plot.add_interval_sets(
+    nap.IntervalSet([10, 80], [20, 100]), colors="purple", alpha=0.3
+)
 
 
 v2 = viz.TsGroupWidget(tsg)
-arg =  [(v.plot.controller, v.plot.renderer), (v2.plot.controller, v2.plot.renderer)]
+arg = [(v.plot.controller, v.plot.renderer), (v2.plot.controller, v2.plot.renderer)]
 
 ctrl_group = ControllerGroup([v.plot, v2.plot])
 
