9f47ce944c9b4c88bb6f5194db661816a710f27a
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-06-10 15:56:02 -0400

    add an example

--- main_simple_glfw.py
+++ main_simple_glfw.py
@@ -2,11 +2,15 @@
 Test script
 """
 import numpy as np
-import os
+import os, sys
 import pynapple as nap
+from PyQt6.QtWidgets import QApplication
 from wgpu.gui.auto import run
 import pynaviz as viz
 from pynaviz.base_plot import _BasePlot
+
+import os
+
 
 
 tsd1 = nap.Tsd(t=np.arange(1000), d=np.sin(np.arange(1000) * 0.1))
@@ -17,14 +21,24 @@ tsg = nap.TsGroup({
 tsdframe = nap.TsdFrame(t=np.arange(1000),d=np.random.randn(1000, 10), metadata={"label":np.random.randn(10)})
 tsdtensor = nap.TsdTensor(t=np.arange(1000), d=np.random.randn(1000, 10, 10))
 
+# video_path = "tests/test_video/numbered_video.mp4"
+# video_path = "/Users/ebalzani/Downloads/20250404_171446-compressed.mp4"
+video_path = "/Users/ebalzani/Downloads/Take 2022-12-13 11.33.25 AM-Camera 5 (#378042) Sofia.avi"
 
+app = QApplication([])
 
-v = viz.PlotTsd(tsd1)
+# v = viz.PlotTsd(tsd1)
 # v = _BasePlot(tsd1)
 # v.canvas.request_draw(v.animate())
 # v = viz.TsdTensorWidget(tsdtensor)
+v = viz.base_plot.PlotVideo(video_path)
+print(v.data.shape)
+
 # v.show()
 # v = viz.TsGroupWidget(tsg)
 # v.show()
 # v = viz.TsdFrameWidget(tsdframe)
 v.show()
+app.exit(app.exec())
+v.close()  # Ensure clean shutdown
+sys.exit(0)
