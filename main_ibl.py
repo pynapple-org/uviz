bc06d3c47d6be54ab132f9b84b1831f556c9a2d9
Guillaume Viejo <guillaume.viejo@gmail.com>
2025-06-30 16:47:43 -0400

    Fix

--- main_ibl.py
+++ main_ibl.py
@@ -1,11 +1,12 @@
 """Stress test: load 125GB NWB file with almost 2H recording and > 1000 units"""
 
 from pathlib import Path
+import os
 
 from one.api import ONE
 from PyQt6.QtWidgets import QApplication
 
-import pynaviz as viz
+import uviz as viz
 
 app = QApplication([])
 
@@ -15,15 +16,15 @@ one = ONE()
 eid = "ebce500b-c530-47de-8cb1-963c552703ea"
 
 # Videos
-ibl_path = Path("")
+ibl_path = Path(os.path.expanduser("~/Downloads"))
 videos = {}
 for label in ["left", "body", "right"]:
     video_path = (
         ibl_path
-        / f"openalyx.internationalbrainlab.org/churchlandlab_ucla/Subjects/MFD_09/2023-10-19/001/raw_video_data/_iblrig_{label}Camera.raw.mp4"
+        / f"ONE/openalyx.internationalbrainlab.org/churchlandlab_ucla/Subjects/MFD_09/2023-10-19/001/raw_video_data/_iblrig_{label}Camera.raw.mp4"
     )
     if not video_path.exists():
-        one.load_dataset(eid, "*leftCamera.raw*", collection="raw_video_data")
+        one.load_dataset(eid, f"*{label}Camera.raw*", collection="raw_video_data")
     times = one.load_object(eid, f"{label}Camera", collection="alf", attribute=["times*"])["times"]
     videos[label] = (times, video_path)
 
@@ -43,10 +44,13 @@ for label in ["left", "body", "right"]:
 # ------------------------------------------------------------------------------------
 # Visualize
 videos = [
-    viz.base_plot.PlotVideo(video_path=video_path, t=times)
+    viz.VideoWidget(video_path=video_path, t=times)
     for times, video_path in videos.values()
 ]
-group = viz.controller_group.ControllerGroup(videos)
+group = viz.controller_group.ControllerGroup([video.plot for video in videos])
+
+for v in videos:
+    v.show()
 
 if __name__ == "__main__":
     app.exit(app.exec())
--- tests/test_video_handler.py
+++ tests/test_video_handler.py
@@ -5,7 +5,7 @@ import imageio.v3 as iio
 import numpy as np
 import pytest
 
-from pynaviz import PlotVideo, video_handling
+from uviz import PlotVideo, video_handling
 
 
 @pytest.fixture()
